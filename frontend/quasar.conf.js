fb6d45d508d9042491b9681b2fe1e0c86b1d4dea
jurelou <louis@jurczyk.fr>
2022-03-06 13:02:39 +0100

    USe hash mod

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -46,7 +46,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       env: {
       },
       // transpile: false,
