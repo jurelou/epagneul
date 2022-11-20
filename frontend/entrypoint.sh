c90c64f8b913d5bae0b58b0e309e4cb1bfad29de
jurelou <louis@jurczyk.fr>
2022-11-20 10:20:05 -0800

    Fix offline deployment

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-quasar build
 quasar serve ./dist/spa --port 8080 --hostname 0.0.0.0 --history
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const api_base_url = window.location.protocol + "//" + window.location.hostname + ":8000/api"
+export const api_base_url = window.location.protocol + "//" + window.location.hostname + ":6327/api"
 
 console.log("Using backend @", api_base_url)
